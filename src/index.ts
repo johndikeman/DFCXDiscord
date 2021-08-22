308720d4a5c056504280bb040a059b2d59bf3b16
John Dikeman <jrobdikeman@gmail.com>
2021-08-21 23:12:15 -0500

    actually we use datastore now 🤪

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 require("dotenv").config();
 import { Client, Intents, Interaction, Message } from "discord.js";
+import { Datastore } from "@google-cloud/datastore";
 
+const datastoreClient = new Datastore({ keyFilename: process.env.IAM_KEY_FILE })
 // const permissionsString = process.env.DISCORD_PERMISSIONS_INTEGER;
 // let discordBitPermissions;
 
@@ -10,18 +12,29 @@ import { Client, Intents, Interaction, Message } from "discord.js";
 //   throw Error("DISCORD_PERMISSIONS_INTEGER not specified in .env, terminating!");
 // }
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.once("ready", () => {
   console.log("client intialised!!");
 })
 
 client.on("messageCreate", async (message: Message) => {
+  // do a test activation phrase
+  if(!message.content.includes("hey test bot")){
+    return;
+  }
   console.log(message.content, message.createdAt, message.author.username);
+  // TODO: make this work with the datastore api
+  datastoreClient.collection("messages").add({
+    content: message.content,
+    createdAt: message.createdAt,
+    senderUsername: message.author.username,
+    senderId: message.author.id
+  })
 })
 
-// client.on("interactionCreate", async (interaction: Interaction) => {
-//   console.log(interaction.type, interaction.user.username,);
-// })
+client.on("interactionCreate", async (interaction: Interaction) => {
+  console.log(interaction.type, interaction.user.username,);
+})
 
 client.login(process.env.DISCORD_BOT_TOKEN);
